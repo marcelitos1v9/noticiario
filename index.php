23b8c6d913613c0f059b0c1d2cca728cd1de2ac8
Marcelo Augusto <123131698+marcelitos1v9@users.noreply.github.com>
2023-05-02 14:21:37 -0300

    Changes to be committed:
    	new file:   imgs/644dd74d033f2.jpg
    	new file:   imgs/644ddbbf15657.png
    	new file:   imgs/644e8057e93eb.png
    	new file:   imgs/644ef77824ea5.png
    	new file:   index blog.txt
    	modified:   index.php
    	modified:   views/blades/footer.php
    	modified:   views/blades/header.php

--- index.php
+++ index.php
@@ -1,37 +1,106 @@
-<?php include("./models/conexao.php")?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Blog</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
+<?php 
+include("./models/conexao.php");
+include("./views/blades/header.php");
+?>
+
 <body>
-    <?php
-    $query = mysqli_query($conexao,"SELECT * FROM blog INNER JOIN blogimgs ON blog_blogimgs_codigo = blogimgs_codigo INNER JOIN bloginfo
-    ON blog_bloginfo_codigo = bloginfo_codigo ORDER BY blog_codigo DESC LIMIT 4");
-    $contador = 0;
-    while($exibe= mysqli_fetch_array($query)){ 
-        if ($contador == 0) {
-            ?>
-            <table border="1" height="250px">
-                <tr>
-                    <td width="200px"><img src="imgs/<?php echo $exibe[5]?>" width="200px"></td>
-                    <td width="400px"><?php echo $exibe[7]?>
-                    <hr>
-                    <a href="page.php<?php echo '?ida='.$exibe[0] ?>"><?php echo $exibe[8]?></a></td>
-                </tr>
-            </table>
-            <?php
-        } else {
-            ?>
-            <a href="page.php<?php echo '?ida='.$exibe[0] ?>"><img src="imgs/<?php echo $exibe[5]?>" width="200px"></a>
-            <?php
-        }
-        $contador++;
-    }?>
-</body>
-</html>
+     <?php
+        $query = mysqli_query($conexao,"SELECT * FROM blog INNER JOIN blogimgs ON blog_blogimgs_codigo = blogimgs_codigo INNER JOIN bloginfo ON blog_bloginfo_codigo = bloginfo_codigo ORDER BY blog_codigo DESC LIMIT 1");
+        $exibe = mysqli_fetch_array($query);
+        ?>
+        <div class="row mb-3">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="imgs/<?php echo $exibe[5]?>" class="card-img main-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body m-auto">
+                                
+                                <h5 class="card-title"><?php echo $exibe[7]?></h5>
+                                <p class="card-text "><?php echo substr($exibe[8], 0, 100)."..."?></p>
+                                <a href="page.php<?php echo '?ida='.$exibe[0] ?>" class="btn btn-primary">Continuar lendo</a>                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Exibição de outras notícias em carrossel -->
+        <?php
+        $query = mysqli_query($conexao,"SELECT * FROM blog INNER JOIN blogimgs ON blog_blogimgs_codigo = blogimgs_codigo INNER JOIN bloginfo ON blog_bloginfo_codigo = bloginfo_codigo WHERE blog_codigo <> $exibe[0] ORDER BY blog_codigo DESC LIMIT 3");
+        ?>
+        <div class="container">
+        <div id="noticias-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <a href="page.php<?php echo '?ida='.$exibe[0] ?>">
+                    <img src="imgs/<?php echo $exibe[5]?>" class="d-block w-100 h-100 carousel-img img-fluid" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5><?php echo $exibe[7]?></h5>
+                            <p><?php echo substr($exibe[8], 0, 100)."..."?></p>
+                        </div>
+                    </a>
+                </div>
+                <?php
+                $contador = 0;
+                while($exibe= mysqli_fetch_array($query)){ 
+                    ?>
+                <div class="carousel-item">
+                    <a href="page.php<?php   echo '?ida='.$exibe[0] ?>">
+                    <img src="imgs/<?php echo $exibe[5]?>" class="d-block w-100 h-75  carousel-img img-fluid" alt="...">
+<div class="carousel-caption d-none d-md-block">
+<h5><?php echo $exibe[7]?></h5>
+<p><?php echo substr($exibe[8], 0, 100)."..."?></p>
+</div>
+</a>
+</div>
+<?php
+             $contador++;
+             }
+             ?>
+</div>
+<a class="carousel-control-prev" href="#noticias-carousel" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Anterior</span>
+</a>
+<a class="carousel-control-next" href="#noticias-carousel" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Próximo</span>
+</a>
+</div>
+</div>
+</div>
+</main>
+<footer class="bg-dark text-white py-4 mt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Contato</h3>
+                <ul class="list-unstyled">
+                    <li>(00) 1234-5678</li>
+                    <li>contato@meusite.com.br</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3>Redes sociais</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Twitter</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3>Localização</h3>
+                <ul class="list-unstyled">
+                    <li>Rua tal, nº 123</li>
+                    <li>Cidade - Estado</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php include("./views/blades/footer.php")?>
