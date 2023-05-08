22067485916dc4539e5cc0633cb18cac3b19604e
Marcelo Augusto <123131698+marcelitos1v9@users.noreply.github.com>
2023-05-07 23:30:45 -0300

    Changes to be committed: 	modified:   controllers/logout.php 	modified:   controllers/ver_login.php 	modified:   index.php

---
+++ controllers/ver_login.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+include("../models/conexao.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    
+    if (empty($email) || empty($senha)) {
+        header("Location: ../views/login.php?erro=Campos_obrigatórios");
+        exit();
+    }
+    
+    if (mysqli_connect_errno()) {
+        header("Location: ../views/ogin.php?erro=Erro_banco");
+        exit();
+    }
+    
+    $query = mysqli_prepare($conexao, "SELECT usuarios_codigo, usuarios_senha FROM usuarios WHERE usuarios_email = ?");
+    mysqli_stmt_bind_param($query, "s", $email);
+    mysqli_stmt_execute($query);
+    $resultado = mysqli_stmt_get_result($query);
+    $usuario = mysqli_fetch_assoc($resultado);
+    
+        if ($usuario && password_verify($senha, $usuario["usuarios_senha"])) {
+        $_SESSION["user_id"] = $usuario["usuarios_codigo"];
+        
+        header("Location: ../index.php?sucesso");
+        exit();
+    } else {
+        header("Location: login.php?erro=Email ou senha incorretos");
+        exit();
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,20 +1,54 @@
 <?php 
 include("./models/conexao.php");
 include("./views/blades/header.php");
+session_start();
+
 ?>
 
 <body>
-     <?php
+<?php
         $query = mysqli_query($conexao,"SELECT * FROM blog INNER JOIN blogimgs ON blog_blogimgs_codigo = blogimgs_codigo INNER JOIN bloginfo ON blog_bloginfo_codigo = bloginfo_codigo ORDER BY blog_codigo DESC LIMIT 1");
         $exibe = mysqli_fetch_array($query);
-        ?>
-        <nav class="navbar fixed-top navbar-light navbar-expand-lg bg-light text-light mb-5 d-flex">
-        <div class="container d-flex justify-content-center">
-            <a class="nav-link" href="./views/login.php">Fazer login</a>
-            <a class="nav-link" href="./views/cadastro.php">Cadastrar</a>
-            <a class="nav-link" href="./controllers/logout.php">Sair</a>
-        </div>
-        </nav>
+
+        if (isset($_SESSION['user_id'])) {
+            $user_id = $_SESSION['user_id'];
+            $query_user = mysqli_query($conexao, "SELECT * FROM usuarios WHERE usuarios_codigo = $user_id");
+            $exibe_user = mysqli_fetch_array($query_user);
+            
+            if ($exibe_user['usuarios_status'] == 2) {
+                echo '<nav class="navbar fixed-top navbar-light navbar-expand-lg bg-light text-light mb-5 d-flex">
+                        <div class="container d-flex justify-content-center">
+                            <a class="nav-link" href="./views/configuracoes.php">Configurações da conta</a>
+                            <a class="nav-link" href="./controllers/logout.php">Sair</a>
+                        </div>
+                    </nav>';
+            } else if ($exibe_user['usuarios_status'] == 1) {
+                echo '<nav class="navbar fixed-top navbar-light navbar-expand-lg bg-light text-light mb-5 d-flex">
+                        <div class="container d-flex justify-content-center">
+                            <a class="nav-link" href="./views/inserir_noticia.php">Inserir nova notícia</a>
+                            <a class="nav-link" href="./views/configuracoes.php">Configurações da conta</a>
+                            <a class="nav-link" href="./controllers/logout.php">Sair</a>
+                        </div>
+                    </nav>';
+            } else if ($exibe_user['usuarios_status'] == 0) {
+                echo '<nav class="navbar fixed-top navbar-light navbar-expand-lg bg-light text-light mb-5 d-flex">
+                        <div class="container d-flex justify-content-center">
+                            <a class="nav-link" href="./views/inserir_noticia.php">Inserir nova notícia</a>
+                            <a class="nav-link" href="./views/configuracoes.php">Configurações da conta</a>
+                            <a class="nav-link" href="./views/controle_usuarios.php">Controle de usuários</a>
+                            <a class="nav-link" href="./controllers/logout.php">Sair</a>
+                        </div>
+                    </nav>';
+            }
+        } else {
+            echo '<nav class="navbar fixed-top navbar-light navbar-expand-lg bg-light text-light mb-5 d-flex">
+                    <div class="container d-flex justify-content-center">
+                        <a class="nav-link" href="./views/login.php">Fazer login</a>
+                        <a class="nav-link" href="./views/cadastro.php">Cadastrar</a>
+                    </div>
+                </nav>';
+        }
+    ?>
         <div class="row mb-3 mt-5">
             <div class="col-md-12">
                 <div class="card">
